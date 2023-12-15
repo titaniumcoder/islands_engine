a4b68c1f18eccca23527316301b77881461e36a7
Rico <rico.metzger@gmail.com>
2023-12-15 02:54:26 +0200

    Starting GenServer

---
+++ lib/islands_engine/game.ex
@@ -0,0 +1,41 @@
+defmodule IslandsEngine.Game do
+  use GenServer
+
+  alias IslandsEngine.Rules
+  alias IslandsEngine.Guesses
+  alias IslandsEngine.Board
+
+  #### Client API ####
+  def start_link(name) when is_binary(name), do: GenServer.start_link(__MODULE__, name, [])
+
+  def add_player(game, name) when is_binary(name), do:
+  GenServer.call(game, {:add_player, name})
+
+  #### Server API ####
+  def init(name) do
+    player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
+    player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
+    {:ok, %{player1: player1, player2: player2, rules: Rules.new()}}
+  end
+
+  def handle_call({:add_player, name}, _from, state) do
+    with {:ok, rules} <- Rules.check(state.rules, :add_player)
+    do
+      state
+      |> update_player2_name(name)
+      |> update_rules(rules)
+      |> reply_success(:ok)
+    else
+      :error -> {:replay, :error, state}
+    end
+  end
+
+  #### Helper Functions ####
+  defp update_player2_name(state, name), do:
+  put_in(state.player2.name, name)
+
+  defp update_rules(state, rules), do:
+  %{state | rules: rules}
+
+  defp reply_success(state, reply), do: {:reply, reply, state}
+end
