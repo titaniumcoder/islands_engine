db2a9ddb17a8ac4f7d2bbe609deff70d6fe858dd
Rico Metzger <rico.metzger@gmail.com>
2023-12-26 19:28:17 +0100

    Still missing tests for the supervisor

--- lib/islands_engine/game_supervisor.ex
+++ lib/islands_engine/game_supervisor.ex
@@ -10,6 +10,7 @@ defmodule IslandsEngine.GameSupervisor do
   def start_game(name), do: DynamicSupervisor.start_child(__MODULE__, {Game, name})
 
   def stop_game(name) do
+    :ets.delete(:game_state, name)
     DynamicSupervisor.terminate_child(__MODULE__, pid_from_name(name))
   end
 
